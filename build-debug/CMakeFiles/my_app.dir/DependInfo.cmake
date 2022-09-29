57a4a4162c416ea04e59d20c2dc87d9d6eb40530
David Rich <david.rich27@gmail.com>
2022-09-29 10:54:58 -0700

    Added launchers, tasks, pytests

--- build-debug/CMakeFiles/my_app.dir/DependInfo.cmake
+++ build-debug/CMakeFiles/my_app.dir/DependInfo.cmake
@@ -8,8 +8,8 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/devreckas/REMOTE-FredHutch/matsen-lab/vscode-example/src/app.cpp" "CMakeFiles/my_app.dir/src/app.cpp.o" "gcc" "CMakeFiles/my_app.dir/src/app.cpp.o.d"
-  "/home/devreckas/REMOTE-FredHutch/matsen-lab/vscode-example/src/math_stuff.cpp" "CMakeFiles/my_app.dir/src/math_stuff.cpp.o" "gcc" "CMakeFiles/my_app.dir/src/math_stuff.cpp.o.d"
+  "/home/drich/matsen-lab/vscode-example-new/src/app.cpp" "CMakeFiles/my_app.dir/src/app.cpp.o" "gcc" "CMakeFiles/my_app.dir/src/app.cpp.o.d"
+  "/home/drich/matsen-lab/vscode-example-new/src/math_stuff.cpp" "CMakeFiles/my_app.dir/src/math_stuff.cpp.o" "gcc" "CMakeFiles/my_app.dir/src/math_stuff.cpp.o.d"
   )
 
 # Targets to which this target links.
